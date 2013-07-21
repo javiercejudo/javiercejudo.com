86a5a1c7dddf00b580cffc5c0fd8d725f6673d63
Javier Cejudo <javier@javiercejudo.com>
2013-07-21 20:20:35 +1000

    adds html5 boilerplate's html tag code to detect ie versions

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php include 'config.php'; ?>
-
 <!DOCTYPE html>
-<html lang="en">
+<!--[if lt IE 7]>      <html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="en" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="en" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
   <head>
     <meta charset="utf-8">
     <title>Javier Cejudo · Web Developer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    
 <?php if (ENV == 'dev') : ?>
     <link href="/bower_components/bootstrap/bootstrap/css/bootstrap.css" rel="stylesheet">
     <link href="/bower_components/bootstrap/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
@@ -16,18 +17,16 @@
 <?php else : ?>
     <link href="/assets/app.css" rel="stylesheet">
 <?php endif; ?>
-    
-<?php if (ENV == 'dev') : ?>
-    <script src="/bower_components/modernizr/modernizr.js"></script>
-<?php else : ?>
-    <script src="/assets/modernizr.js"></script>
-<?php endif; ?>
-    
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72"   href="/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed"                 href="/ico/apple-touch-icon-57-precomposed.png">
     <link rel="shortcut icon"                                href="/ico/favicon.png">
+<?php if (ENV == 'dev') : ?>
+    <script src="/bower_components/modernizr/modernizr.js"></script>
+<?php else : ?>
+    <script src="/assets/modernizr.js"></script>
+<?php endif; ?>
   </head>
 
   <body ng-app="JcApp" ng-controller="AppCtrl">
