941e175377b8d113e1da00d2024d4f450ab95847
Javier Cejudo <javier@javiercejudo.com>
2013-10-27 23:42:26 +1100

    JsHint config file to support Jasmine syntax and simplifies travis setup

--- js/AppFilters.js
+++ js/AppFilters.js
@@ -4,7 +4,7 @@
   'use strict';
 
   angular.module('JcApp')
-  
+
     .filter('jcCapitalise', [function () {
       return function (string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
--- tests/unit/SecretaryProblemSpec.js
+++ tests/unit/SecretaryProblemSpec.js
@@ -1,49 +1,49 @@
-'use strict';
+(function () {
+  'use strict';
 
-describe("Secretary Problem suite", function() {
-  var scope, controller;
+  describe("Secretary Problem suite", function() {
+    var scope, controller;
 
-  beforeEach(module('SecretaryProblem'));
+    beforeEach(module('SecretaryProblem'));
 
-  beforeEach(inject(function($injector) {
+    beforeEach(inject(function($injector) {
       scope = $injector.get('$rootScope');
       controller = $injector.get('$controller');
 
       // instantiate the controller
       controller('SecretaryProblemCtrl', {$scope: scope});
-  }));
+    }));
 
-  describe('automaticGame', function () {
-    it('should achieve a ~1/e success rate with n big', function () {
-      var numberOfItems = 256;
-      var numberOfGames = 1000; // max: 22500
-      var expectedRate = 1 / Math.E;
-      var acceptedMargin = .05;
-      var successRate = scope.automaticGame(numberOfItems, numberOfGames);
-      var rateDifference = Math.abs(successRate - expectedRate);
+    describe('automaticGame', function () {
+      it('should achieve a ~1/e success rate with n big', function () {
+        var numberOfItems = 256;
+        var numberOfGames = 1000; // max: 22500
+        var expectedRate = 1 / Math.E;
+        var acceptedMargin = 0.075;
+        var successRate = scope.automaticGame(numberOfItems, numberOfGames);
+        var rateDifference = Math.abs(successRate - expectedRate);
 
-      console.info('numberOfItems', numberOfItems);
-      console.info('numberOfGames', numberOfGames);
-      console.info('successRate', successRate);
+        // console.info('numberOfItems', numberOfItems);
+        // console.info('numberOfGames', numberOfGames);
+        // console.info('successRate', successRate);
 
-      expect(rateDifference).toBeLessThan(acceptedMargin);
+        expect(rateDifference).toBeLessThan(acceptedMargin);
+      });
+
+      it('should achieve a ~1/2 success rate with n=3', function () {
+        var numberOfItems = 3;
+        var numberOfGames = 1000; // max: 500000
+        var expectedRate = 0.5;
+        var acceptedMargin = 0.05;
+        var successRate = scope.automaticGame(numberOfItems, numberOfGames);
+        var rateDifference = Math.abs(successRate - expectedRate);
+
+        // console.info('numberOfItems', numberOfItems);
+        // console.info('numberOfGames', numberOfGames);
+        // console.info('successRate', successRate);
+
+        expect(rateDifference).toBeLessThan(acceptedMargin);
+      });
     });
   });
-
-  describe('automaticGame', function () {
-    it('should achieve a ~1/2 success rate with n=3', function () {
-      var numberOfItems = 3;
-      var numberOfGames = 1000; // max: 500000
-      var expectedRate = .5;
-      var acceptedMargin = .03;
-      var successRate = scope.automaticGame(numberOfItems, numberOfGames);
-      var rateDifference = Math.abs(successRate - expectedRate);
-
-      console.info('numberOfItems', numberOfItems);
-      console.info('numberOfGames', numberOfGames);
-      console.info('successRate', successRate);
-
-      expect(rateDifference).toBeLessThan(acceptedMargin);
-    });
-  });
-});
+}());
