fc66be633a6ddf382442d5534928346cddbb9152
Javier Cejudo <javier@javiercejudo.com>
2014-04-24 23:26:47 +1000

    Modify Goggle UA tracking snippet

--- layout/foot.php
+++ layout/foot.php
@@ -42,12 +42,11 @@
   <script type="text/javascript" src="//dl1d2m8ri9v3j.cloudfront.net/releases/1.2.1/tracker.js" data-customer="6f626efb34174c6885aef28b406d6bab"></script>
   <script src="<?php echo $build("bottom", "js") ?>"></script>
   <script>
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-    e.src='//www.google-analytics.com/analytics.js';
-    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    ga('create','UA-43072086-1');
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-43072086-1', 'www.javiercejudo.com');
     ga('require', 'displayfeatures');
   </script>
   <script>
