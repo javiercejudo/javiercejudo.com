d3b443adb20b8d3873f70403919736d591e5fab9
Javier Cejudo <javier@javiercejudo.com>
2014-04-28 01:51:40 +1000

    Adds PHP script to backup firebase data

---
+++ scripts/firebase-backup.php
@@ -0,0 +1,33 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+
+// download Firebase export and store it locally
+$firebaseExport = 'https://c3jud0.firebaseio.com/.json?format=export';
+$localBackup = __DIR__ . '/../data/min/c3jud0-export.json';
+$result = file_put_contents($localBackup, file_get_contents($firebaseExport));
+
+// if the local file is not created, something went wrong and we can stop
+if (false === $result) {
+    exit();
+}
+
+$s3Client = S3Client::factory(array(
+    'key'    => getenv('S3_KEY'),
+    'secret' => getenv('S3_SECRET')
+));
+
+// upload export to S3
+// (the file size, file type, and MD5 hash are automatically calculated by the SDK)
+try {
+    $s3Client->putObject(array(
+        'Bucket' => 'jc-firebase',
+        'Key'    => date('c', time()) . '.json',
+        'Body'   => fopen($localBackup, 'r'),
+        'ACL'    => 'public-read',
+    ));
+} catch (Exception $e) {
+    echo "There was an error uploading the file." . PHP_EOL;
+}
