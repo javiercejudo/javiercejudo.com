36445bab4c12485d1d508d3f52ca8adfa66938b7
Javier Cejudo <javier@javiercejudo.com>
2014-05-16 08:41:05 +1000

    Reduce costly tests that are redundant

--- tests/unit/SecretaryProblemSpec.js
+++ tests/unit/SecretaryProblemSpec.js
@@ -30,31 +30,31 @@
         expect(successRate).toBe(null);
       });
 
-      it('should win and lose at least a game with n unrealistically big', function () {
-        spyOn(scope, 'findBestStrategy').and.returnValue(2759);
+      // it('should win and lose at least a game with n unrealistically big', function () {
+      //   spyOn(scope, 'findBestStrategy').and.returnValue(2759);
 
-        var
-          numberOfItems = 7500,
-          numberOfGames = 15,
-          successRate = scope.automaticGame(numberOfItems, numberOfGames);
+      //   var
+      //     numberOfItems = 7500,
+      //     numberOfGames = 15,
+      //     successRate = scope.automaticGame(numberOfItems, numberOfGames);
 
-        expect(successRate).toBeGreaterThan(0);
-        expect(successRate).toBeLessThan(1);
-      });
+      //   expect(successRate).toBeGreaterThan(0);
+      //   expect(successRate).toBeLessThan(1);
+      // });
 
-      it('should achieve a ~1/e success rate with n big', function () {
-        spyOn(scope, 'findBestStrategy').and.returnValue(94);
+      // it('should achieve a ~1/e success rate with n big', function () {
+      //   spyOn(scope, 'findBestStrategy').and.returnValue(94);
 
-        var
-          numberOfItems = 256,
-          numberOfGames = 1000, // max: 22500
-          expectedRate = 1 / Math.E,
-          acceptedMargin = 0.075,
-          successRate = scope.automaticGame(numberOfItems, numberOfGames),
-          rateDifference = Math.abs(successRate - expectedRate);
+      //   var
+      //     numberOfItems = 256,
+      //     numberOfGames = 1000, // max: 22500
+      //     expectedRate = 1 / Math.E,
+      //     acceptedMargin = 0.075,
+      //     successRate = scope.automaticGame(numberOfItems, numberOfGames),
+      //     rateDifference = Math.abs(successRate - expectedRate);
 
-        expect(rateDifference).toBeLessThan(acceptedMargin);
-      });
+      //   expect(rateDifference).toBeLessThan(acceptedMargin);
+      // });
 
       it('should achieve ~20% success rate with n=5 if selecting the first box', function () {
         spyOn(scope, 'findBestStrategy').and.returnValue(0);
@@ -133,42 +133,42 @@
               18      : 6,
               19      : 7,
               20      : 7,
-              271     : 100,
-              543     : 200,
-              815     : 300,
-              1087    : 400,
-              1359    : 500,
-              1631    : 600,
-              1902    : 700,
-              2174    : 800,
-              2446    : 900,
-              2718    : 1000,
-              5436    : 2000,
-              8154    : 3000,
-              10873   : 4000,
-              13591   : 5000,
-              16309   : 6000,
-              19028   : 7000,
-              21746   : 8000,
-              24464   : 9000,
-              27182   : 10000,
-              54365   : 20000,
-              81548   : 30000,
-              108731  : 40000,
-              135914  : 50000,
-              163097  : 60000,
-              190279  : 70000,
-              217462  : 80000,
-              244645  : 90000,
-              271828  : 100000,
-              543656  : 200000,
-              815484  : 300000,
-              1087312 : 400000,
-              1359141 : 500000,
-              1630969 : 600000,
-              1902797 : 700000,
-              2174625 : 800000,
-              2446453 : 900000,
+              // 271     : 100,
+              // 543     : 200,
+              // 815     : 300,
+              // 1087    : 400,
+              // 1359    : 500,
+              // 1631    : 600,
+              // 1902    : 700,
+              // 2174    : 800,
+              // 2446    : 900,
+              // 2718    : 1000,
+              // 5436    : 2000,
+              // 8154    : 3000,
+              // 10873   : 4000,
+              // 13591   : 5000,
+              // 16309   : 6000,
+              // 19028   : 7000,
+              // 21746   : 8000,
+              // 24464   : 9000,
+              // 27182   : 10000,
+              // 54365   : 20000,
+              // 81548   : 30000,
+              // 108731  : 40000,
+              // 135914  : 50000,
+              // 163097  : 60000,
+              // 190279  : 70000,
+              // 217462  : 80000,
+              // 244645  : 90000,
+              // 271828  : 100000,
+              // 543656  : 200000,
+              // 815484  : 300000,
+              // 1087312 : 400000,
+              // 1359141 : 500000,
+              // 1630969 : 600000,
+              // 1902797 : 700000,
+              // 2174625 : 800000,
+              // 2446453 : 900000,
               2718281 : 1000000,
               5436563 : 2000000
             };
