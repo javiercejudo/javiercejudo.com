a5d6e64fcb62e10b02f8f8ba47c8a9e6f3b475d8
Javier Cejudo <javier@javiercejudo.com>
2014-05-29 08:05:33 +1000

    Modifies wraith script to work in dev

--- scripts/publish_wraith.sh
+++ scripts/publish_wraith.sh
@@ -2,6 +2,9 @@
 
 if [ "$TRAVIS_BRANCH" = "staging" ]; then
     gem install wraith
+fi
+
+if [ "$TRAVIS_BRANCH" = "staging" ] || [ "$ENV" = "dev" ]; then
     cd wraith/
     wraith capture live-vs-staging
     cd ..
