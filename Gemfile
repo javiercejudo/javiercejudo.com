29354e16485062b4e25374090e9e6706d93e4107
Javier Cejudo <javier@javiercejudo.com>
2014-07-31 23:30:23 +1000

    Dependency upgrade and cleanup

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source "https://rubygems.org"
 
-gem "travis", '~> 1.6.16'
+gem "travis", '~> 1.6.17'
 gem 'wraith', '~> 1.3.0'
--- layout/foot.php
+++ layout/foot.php
@@ -39,7 +39,7 @@
   <script src="js/controllers/CvCtrl.js"></script>
   <script src="js/controllers/SecretaryProblemCtrl.js"></script>
 <?php else : ?>
-  <script src="<?php echo $build("bottom", "js") ?>" async></script>
+  <script src="<?php echo $build("bottom", "js") ?>"></script>
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
