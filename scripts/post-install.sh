4c713bca5760f1f116f455a12f74b0e71c7f8959
Javier Cejudo <javier@javiercejudo.com>
2014-08-01 21:18:00 +1000

    Fix build publication

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -3,6 +3,6 @@
 bower install
 gulp
 
-if [ "$CONTINUOUS_INTEGRATION" = "true" ] || ["$ENV" = "live" ]; then
+if [ "$CONTINUOUS_INTEGRATION" = "true" ] || [ "$ENV" = "live" ]; then
     gulp publish-build
 fi
