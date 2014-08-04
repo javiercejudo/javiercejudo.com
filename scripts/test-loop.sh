2059475cb0cfe0ba54dfaccdce0843567a860885
Javier Cejudo <javier@javiercejudo.com>
2014-08-05 00:21:21 +1000

    Simplify test loop script

--- scripts/test-loop.sh
+++ scripts/test-loop.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
 SUCCESS=0;
-FAILURES=0;
-TOTAL=0;
+COUNT=0;
 ITERATIONS=1;
 
 if [ -n "$1" ]; then
     let ITERATIONS=$1
 fi
 
-while [ $TOTAL -lt $ITERATIONS ]; do
-    let TOTAL+=1
+while [ $COUNT -lt $ITERATIONS ]; do
+    let COUNT+=1
 
     if grunt karma:dev; then
         let SUCCESS+=1
-    else
-        let FAILURES+=1
     fi
 
-    echo Success rate: $SUCCESS/$TOTAL
+    echo Success rate: $SUCCESS/$COUNT
 done
 
 if hash notify-send 2>/dev/null; then
-    notify-send "Test loop" "Done ($SUCCESS/$TOTAL)"
+    notify-send "JavierCejudo.com test loop" "Success rate: $SUCCESS/$ITERATIONS"
 fi
