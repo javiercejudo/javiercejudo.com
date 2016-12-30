9b3836171bff3e7f18aa59122e934b768373e5c4
Javier Cejudo <javier.cejudo@skiddoo.com.au>
2016-12-30 21:15:47 +1100

    chore(env): use latest node

--- scripts/local-server.sh
+++ scripts/local-server.sh
@@ -16,8 +16,8 @@ function jcecho {
 jcecho "Starting application..."
 
 if [[ $1 == "live" ]]
-then sudo foreman start --env=.env.live
-else sudo foreman start
+then sudo nf start --env=.env.live
+else sudo nf start
 fi
 
 jcecho "Goodbye!"
