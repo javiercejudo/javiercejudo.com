e661a951324053605cd72dead23155dbb7f17e38
Javier Cejudo <javier@javiercejudo.com>
2021-03-14 03:09:42 +1100

    wip59

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -3,3 +3,7 @@
 set -x
 
 npx nf run ./scripts/build-pages.js
+
+mv molino-registry.txt molino-registry-2.txt
+npx neek -i molino-registry-2.txt -o molino-registry.txt
+rm -f molino-registry-2.txt
--- src/pages/menu/build.js
+++ src/pages/menu/build.js
@@ -1,14 +1,18 @@
 const path = require('path');
+const blogData = require('../blog/data');
 
 const buildContact = async ({buildPage}) =>
   await buildPage({
-    pageSourcePath: path.join(__dirname, 'template.html'),
+    pageSourcePath: path.join(__dirname, 'template.mustache'),
     relativeOutputPath: path.join('menu', 'index.html'),
     layoutData: {
       title: 'Menu - javiercejudo.com',
       description: 'Find your way around my site',
       pageIsMenu: true,
       styles: ['menu/index.css'],
+    },
+    pageData: {
+      blogPath: blogData.path,
     },
   });
 
